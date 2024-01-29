00d9267c997c139c908b9a6c6f9e64f9845110cd
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-01-29 14:39:40 +0400

    updated database problem

--- back/back/settings.py
+++ back/back/settings.py
@@ -81,10 +81,6 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
