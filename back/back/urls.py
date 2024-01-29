8d335a22be66827abd407b9e80808a2fb5904a22
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-01-29 14:56:39 +0400

    updated settings,  urls

--- back/back/urls.py
+++ back/back/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('questions/', include('questions.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
